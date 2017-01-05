dc361fc40b0b429d198dccb33d17ad4adf7129f2
yangyi <639293382@qq.com>
2017-01-05 11:33:56 +0800

    new

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,6 +19,7 @@ module.exports = {
     // allow debugger during development
     'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
     'semi':['error','always'],
-    'indent':0
+    'indent':0,
+    'space-before-function-paren': 0
   }
 };
